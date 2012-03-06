639830c8bb5ec15147d8c3721130f549c3a7846c
Grégoire Morpain <gregoire.morpain@gmail.com>
2012-03-06 00:54:29 -0500

    +dropping pqsort

--- src/pqsort/pqsort.c
+++ src/pqsort/pqsort.c
@@ -6,12 +6,7 @@
 //  (c) Copyright 2012 Grégoire Morpain. All rights reserved.
 //
 
-#include <unistd.h>
-#include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <strings.h>
-#include <math.h>
 #include <mpi.h>
 #include "comp428a2.h"
 #include "pqsort.h"
@@ -20,14 +15,10 @@ int main(int argc, const char *argv[])
 {
   int id, p;
   
-  id = 0;
-  p = 0;
-  
   MPI_Init(&argc, (char***)&argv);
   MPI_Comm_rank(MPI_COMM_WORLD, &id);
   MPI_Comm_size(MPI_COMM_WORLD, &p);
 
-  // int MPI_Cart_create(MPI_Comm comm_old, int ndims, int *dims, int *periods, int reorder, MPI_Comm *comm_cart)
   int ndims = 3;
   int dims[3] = {1, 1, 1};
   int periods[3] = {1, 1, 1};
@@ -35,13 +26,6 @@ int main(int argc, const char *argv[])
   MPI_Comm comm_cart;
 
   MPI_Cart_create(MPI_COMM_WORLD, ndims, dims, periods, reorder, &comm_cart);
-
-  // int mycoords[3];
-  // 
-  // // int MPI_Cart_coords(MPI_Comm comm, int rank, int maxdims, int *coords)
-  // MPI_Cart_coords(comm_cart, id, 3, mycoords);
-  // 
-  // printf("P[%d, %d, %d]\n", mycoords[0], mycoords[1], mycoords[2]);
 
   MPI_Finalize();
   return EXIT_SUCCESS;
