5eb6663803a1b9d2675596bc7f5666f0d08f6531
Grégoire Morpain <gregoire.morpain@gmail.com>
2012-03-05 23:31:37 -0500

    +hypercube first test

--- src/pqsort/pqsort.c
+++ src/pqsort/pqsort.c
@@ -6,11 +6,37 @@
 //  (c) Copyright 2012 Grégoire Morpain. All rights reserved.
 //
 
+#include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <strings.h>
+#include <math.h>
+#include <mpi.h>
+#include "comp428a2.h"
+#include "pqsort.h"
 
-int main()
+int main(int argc, const char *argv[])
 {
-	printf("pqsort\n");
+  int id, p;
+  
+  id = 0;
+  p = 0;
+  
+  MPI_Init(&argc, (char***)&argv);
+  MPI_Comm_rank(MPI_COMM_WORLD, &id);
+  MPI_Comm_size(MPI_COMM_WORLD, &p);
+
+  // int MPI_Cart_create(MPI_Comm comm_old, int ndims, int *dims, int *periods, int reorder, MPI_Comm *comm_cart)
+  int ndims = 4;
+  int dims[4] = {2, 2, 2, 2};
+  int periods[4] = {1, 1, 1, 1};
+  int reorder = 1;
+  MPI_Comm comm_cart;
+    
+  MPI_Cart_create(MPI_COMM_WORLD, ndims, dims, periods, reorder, &comm_cart);
+
+  MPI_Finalize();
+
   return EXIT_SUCCESS;
 }
---
+++ src/pqsort/pqsort.h
@@ -0,0 +1,15 @@
+//
+//  pqsort.h
+//  COMP428-A2
+//
+//  Created by Grégoire Morpain on 2012-03-05.
+//  (c) Copyright 2012 Grégoire Morpain. All Rights Reserved.
+//
+
+
+#ifndef __PQSORT_H__
+#define __PQSORT_H__
+
+#define ROOT          0
+
+#endif
